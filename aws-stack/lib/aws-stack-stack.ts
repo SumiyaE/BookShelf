8449442d797f989b942a22d43b83d35be44e8114
sumiyaE <s_eguchi@codmon.co.jp>
2023-06-04 15:50:01 +0900

    dynamoDBのスタックを追加

--- aws-stack/lib/aws-stack-stack.ts
+++ aws-stack/lib/aws-stack-stack.ts
@@ -1,19 +1,33 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
 
 export class AwsStackStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    //
+    // const queue = new sqs.Queue(this, 'AwsStackQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
+    //
+    // const topic = new sns.Topic(this, 'AwsStackTopic');
+    //
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+    const tablle = new Table(this,"item",{
+      partitionKey : {
+        name : "ISBN",
+        type : AttributeType.STRING
+      },
+      tableName: "items",
+      removalPolicy: RemovalPolicy.DESTROY
+    })
 
-    const queue = new sqs.Queue(this, 'AwsStackQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'AwsStackTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const putItemFunction = new NodejsFunction(this, 'putItemFunction', {
+      entry : "../lambda/putItem.ts",
+      runtime : Runtime.NODEJS_18_X,
+      handler : "putItem"
+    })
   }
 }
