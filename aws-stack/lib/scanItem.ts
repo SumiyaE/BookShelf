86a8e0622fac5ca5c5ce1dfc4b8b267a8e5f78bd
sumiyaE <s_eguchi@codmon.co.jp>
2023-06-12 19:31:00 +0900

    Usecaseから呼び出すように編集

--- aws-stack/lib/scanItem.ts
+++ aws-stack/lib/scanItem.ts
@@ -1,12 +1,14 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { APIGatewayProxyResult } from "aws-lambda";
 import { ItemRepository } from "./Repository/ItemRepository";
+import { GetItem } from "./Usecase/getItem";
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
     // DynamoDBテーブルの名前は環境変数から取得する
     const ddbClient = new DynamoDBClient({});
-    const itemRepository = new ItemRepository(ddbClient)
-    const scanData = await itemRepository.scanItem();
+    const itemRepository = new ItemRepository(ddbClient);
+    const getItem = new GetItem(itemRepository);
+    const scanData = await getItem.getAllItems();
     return {
         statusCode: 200,
         headers: {
